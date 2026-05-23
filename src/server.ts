be81e5cb4e124385d56373c133e7cf55e6f365e9
eduardoqsilva <eduadsilva@outlook.com>
2026-05-23 16:15:07 -0300

    add a .env.exemple and  biome to lint create migrations and  configure knex and create first routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
+import { env } from './env'
+import transactionsRoutes from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', () => 'hello world')
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('🔥 HTTP server running on http://localhost:3333')
+    console.log(`🔥 HTTP server running on http://localhost:${env.PORT}`)
   })
